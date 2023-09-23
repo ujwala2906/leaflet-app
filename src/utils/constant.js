9e784b9824fa559526c4cc7ef10293e4a80ede35
ujwala2906 <ujwala.dhanule@successive.tech>
2023-09-24 01:49:59 +0530

    Edit control and custom control

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,10 @@
 export const OPEN_STREET_MAP_SEARCH_URL =
   "https://nominatim.openstreetmap.org/search?";
+
+export const mapLayers = {
+  openStreet:
+    "https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=8ZkOhU4g5rv4NOheXZ6F",
+  satelite:
+    "https://api.maptiler.com/maps/satellite/256/{z}/{x}/{y}.jpg?key=8ZkOhU4g5rv4NOheXZ6F",
+  topo: "https://api.maptiler.com/maps/topo-v2/256/{z}/{x}/{y}.png?key=8ZkOhU4g5rv4NOheXZ6F",
+};
